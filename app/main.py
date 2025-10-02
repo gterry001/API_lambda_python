2746434de7830c709e139fa5e5d891eb36c27b75
gterry001 <72092025+gterry001@users.noreply.github.com>
2025-10-02 19:03:17 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ def handler(event, context):
             print(f"👉 Procesando job {job_id}")
 
             # Ejecuta tu análisis
-            porfolio,df_betas = run_portfolio_analysis()
+            portfolio,df_betas = run_portfolio_analysis()
             
             # Generar datos para dashboard
             dashboard_data = prepare_dashboard_data(portfolio, df_betas)
